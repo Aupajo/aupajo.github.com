6a0963458b9301b8d3bacc3a9dbccc79a6e33e65
Pete Nicholls <pete@metanation.com>
2015-01-18 14:36:03 +1300

    Travis: Deploy on successful build

--- config.rb
+++ config.rb
@@ -2,6 +2,11 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.remote = 'git@github.com:Aupajo/aupajo.github.io.git'
+end
+
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
